797795a429f9ac67a20f8b0b69553b1b80c1ab46
Robin Parisi <parisi.robin@gmail.com>
2016-05-03 15:09:28 +0200

    add configurable analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,9 @@
 
     @yield('css')
     <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
+    @if(App::environment() == 'prod')
+    @include('elements.marketing.analytics')
+    @endif
 </head>
 <body>
     <div class="container">
@@ -36,14 +39,5 @@
     <script src="{{ asset('bower/jquery/dist/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/js/bundle.js') }}"></script>
     @yield('js')
-
-    <script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='https://www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-    </script>
 </body>
 </html>
