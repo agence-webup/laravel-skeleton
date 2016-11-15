fa1ef64911b319a08d1f9f618b4f8042c26251ed
Théotix <hobbes@theoti.me>
2016-11-15 16:39:02 +0100

    Add success command layout template

--- resources/views/cart/success.blade.php
+++ resources/views/cart/success.blade.php
@@ -1,18 +1,27 @@
 <?php
-SEO::setTitle('Home');
-SEO::setDescription('Home page description');
+SEO::setTitle('Confirmation de la commande');
+SEO::setDescription('Commande effectuée avec succès');
 ?>
 
 @extends('layouts.master')
 @section('content')
+<div class="container">
+    @include('elements.steps', ['step' => 4])
+    <div class="container container--small txtcenter">
+        <h1>Merci de votre commande !</h1>
 
-<div class="">
-    Merci de votre commande
+        <p>Un e-mail récapitulatif a été envoyé à l’adresse : martin.durand@fai.com.<br/>
+        Le courrier peut mettre plusieurs minutes à arriver.</p>
+        <p>Si vous ne recevez aucun message, nous vous invitons à consulter votre dossier “courriers indésirables”.</p>
+        <p><strong>Votre commande va être traitée dans les plus brefs délais.</strong></p>
+
+        <div class="cart-box">
+            <p>Nous vous invitons à vous connecter à votre compte XXX pour effectuer le suivi de la commande.</p>
+            <p>Si votre adresse e-mail est erronée, nous vous invitons à vous connecter à l’aide du lien ci-dessous pour le corriger.</p>
+            <a class="btn btn--primary" href="{{ route('customer.dashboard') }}">J'accède à mon compte</a>
+        </div>
+
+        <a href="{{ route('home') }}">Retourner sur le site</a>
+    </div>
 </div>
-
-
-<div class="">
-    <a href="{{ route('customer.dashboard') }}">J'accède à mon compte</a>
-</div>
-<a href="{{ route('home') }}">Retourner sur le site</a>
 @endsection
