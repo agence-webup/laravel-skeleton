a5cf9c9a9d389a067f3464bb6bdc2e2f24f625b4
Théotix <hobbes@theoti.me>
2016-11-23 12:19:01 +0100

    Add breadcrumb module for catalog layout

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -7,11 +7,31 @@ SEO::setDescription('Home page description');
 @section('content')
 <h1>Liste de produits</h1>
 
-<div class="">
-    @foreach ($products as $product)
-    <div class="">
-        <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">{{ $product->name }}</a>
+<div class="container">
+    <div class="catalog__breadcrumb">
+        <ul class="breadcrumb">
+            <li class="breadcrumb__listiItem">
+                <a href="" class="breadcrumb__itemContent">Item</a>
+            </li>
+            <li class="breadcrumb__listiItem">
+                <a href="" class="breadcrumb__itemContent">Item</a>
+            </li>
+            <li class="breadcrumb__listiItem">
+                <a href="" class="breadcrumb__itemContent">Item</a>
+            </li>
+            <li class="breadcrumb__listiItem">
+                <span class="breadcrumb__itemContent">Current item</span>
+            </li>
+        </ul>
     </div>
-    @endforeach
+
+    <div class="">
+        @foreach ($products as $product)
+        <div class="">
+            <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">{{ $product->name }}</a>
+        </div>
+        @endforeach
+    </div>
+
 </div>
 @endsection
