557870b65e2af240d0630f4e25fc88fe84ae2d18
Robin Parisi <parisi.robin@gmail.com>
2017-02-20 17:39:40 +0100

    Update seotools.php
    SEO config

--- config/seotools.php
+++ config/seotools.php
@@ -7,7 +7,7 @@ return [
          */
         'defaults'       => [
             'title'        => false, // set false to total remove
-            'description'  => 'SITE_DESCRIPTION', // set false to total remove
+            'description'  => false, // set false to total remove
             'separator'    => ' - ',
             'keywords'     => [],
             'canonical'    => null, // Set null for using Url::current(), set false to total remove
@@ -29,8 +29,8 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'SITE_NAME', // set false to total remove
-            'description' => 'SITE_DESCRIPTION', // set false to total remove
+            'title'       => false, // set false to total remove
+            'description' => false, // set false to total remove
             'url'         => false,
             'type'        => false,
             'site_name'   => false,
