8c531e4cc823194f7a516fbfee58db29eab8d842
Baptiste Ducatel <baptiste@agence-webup.com>
2020-03-31 16:45:48 +0200

    Update to Laravel 7

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -11,12 +11,12 @@ return [
     | that are given by the password broker for a password update attempt
     | has failed, such as for an invalid token or invalid new password.
     |
-     */
+    */
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "We can't find a user with that email address.",
 
 ];
