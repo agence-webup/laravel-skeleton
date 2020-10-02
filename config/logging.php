8046a6df7b9acf20e45faf425244a45141aab6cb
Baptiste Ducatel <baptiste@agence-webup.com>
2020-10-02 18:43:47 +0200

    Update to laravel 8

--- config/logging.php
+++ config/logging.php
@@ -15,7 +15,7 @@ return [
     | messages to the logs. The name specified in this option should match
     | one of the channels defined in the "channels" configuration array.
     |
-     */
+    */
 
     'default' => env('LOG_CHANNEL', 'stack'),
 
@@ -32,7 +32,7 @@ return [
     |                    "errorlog", "monolog",
     |                    "custom", "stack"
     |
-     */
+    */
 
     'channels' => [
         'stack' => [
