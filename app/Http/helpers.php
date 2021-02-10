37cfaf345d574b8415023fdae133eb2024a49081
Brybry <bryan@agence-webup.com>
2021-02-10 12:02:48 +0100

    Update assets function

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -6,55 +6,57 @@ if (!function_exists('asset')) {
      * Override the Laravel's asset function
      * Generate a URL to an application asset,
      * and suffix the file with its checksum for cache busting.
-     *
-     * @param  string  $path
-     * @param  bool|null  $secure
-     * @return string
      */
     function asset($file, $buildDirectory = '')
     {
+        $assetUrl = trim(config('app.asset_url'), '/');
+
         static $manifest = [];
         static $manifestPath;
-        $isNodemodule = strpos($file, 'node_modules') !== false;
-
-        if ($isNodemodule) {
-            //get package name from path (ex "library" from "/node_modules/librabry/dist/library.css")
-            $matches = [];
-            preg_match("/\/node_modules\/(.+)\//mU", $file, $matches);
-            $packageName = $matches[1] ?? null;
-
-            $nodeModuleVersionsPath = storage_path("framework/node_module_versions.php");
-            if (!file_exists($nodeModuleVersionsPath)) {
-                throw new InvalidArgumentException('File "' . $nodeModuleVersionsPath . '" doesn\'t exist. Did you run "php artisan nodemodules:versions" ?');
-            }
-            $nodeModuleVersions = unserialize(file_get_contents($nodeModuleVersionsPath));
-
-            try {
-                $packageVersion = $nodeModuleVersions[$packageName];
-            } catch (\Throwable $th) {
-                throw new InvalidArgumentException("Cannot find version for package " . $packageName);
-            }
-            $publicPath = env('ASSETS_URL') . '/' . trim($file, '/');
-
-            return $publicPath . "?v=" . $packageVersion;
-        } elseif (empty($manifest) || $manifestPath !== $buildDirectory) {
+        if (empty($manifest) || $manifestPath !== $buildDirectory) {
             $path = public_path($buildDirectory . '/rev-manifest.json');
             if (file_exists($path)) {
                 $manifest = json_decode(file_get_contents($path), true);
                 $manifestPath = $buildDirectory;
             }
         }
-        $file = ltrim($file, '/');
-        if (isset($manifest[$file])) {
-            $publicPath = config('app.asset_url') . '/' . trim($buildDirectory . '/' . $manifest[$file], '/');
-            return $publicPath;
+
+        static $manifestWebpack = [];
+        static $manifestWebpackPath;
+        if (empty($manifestWebpack) || $manifestWebpackPath !== $buildDirectory) {
+            $path = public_path($buildDirectory . '/mix-manifest.json');
+            if (file_exists($path)) {
+                $manifestWebpack = json_decode(file_get_contents($path), true);
+                $manifestWebpackPath = $buildDirectory;
+            }
+        }
+
+        static $manifestNpm = [];
+        static $manifestNpmPath;
+        if (empty($manifestNpm) || $manifestNpmPath !== $buildDirectory) {
+            $path = public_path($buildDirectory . '/npm-manifest.json');
+            if (file_exists($path)) {
+                $manifestNpm = json_decode(file_get_contents($path), true);
+                $manifestNpmPath = $buildDirectory;
+            }
+        }
+
+        $trimmedFile = ltrim($file, '/');
+        if (isset($manifestNpm[$file])) {
+            return $assetUrl . '/' . trim($buildDirectory . '/' . $manifestNpm[$file], '/');
+        } elseif (isset($manifestWebpack[$file])) {
+            return $assetUrl . '/' . trim(mix($file), "/");
+        } elseif (isset($manifest[$trimmedFile])) {
+            return $assetUrl . '/' . trim($buildDirectory . '/' . $manifest[$trimmedFile], '/');
         }
 
         // remove query string
         $unversioned = public_path(parse_url($file, PHP_URL_PATH));
 
+
+
         if (file_exists($unversioned)) {
-            $publicPath = config('app.asset_url') . '/' . trim($file, '/');
+            $publicPath = $assetUrl . '/' . trim($file, '/');
             return $publicPath;
         }
 
