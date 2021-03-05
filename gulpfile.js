72157abbe62925e0d5f292fe0accf472c7784b42
Robin PARISI <parisi.robin@gmail.com>
2021-03-05 19:28:45 +0100

    Upgrade gulpy

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,33 @@ const Gulpy = require('@agence-webup/gulpy')
 // config
 const gulpy = new Gulpy({
   publicFolder: 'public',
-  manifest: 'public/rev-manifest.json'
+  manifest: 'public/rev-manifest.json',
+  npmManifest: 'public/npm-manifest.json',
+  mozjpeg: {
+    progressive: true,
+    quality: 80
+  }
 })
 
 // front
 const sass = gulpy.sass('resources/assets/sass/style.scss', 'public/assets/css')
-const js = gulpy.js(['resources/assets/js/**/*', '!src/js/*.js'], 'public/assets/js')
+const js = gulpy.js(['resources/assets/js/**/*.js', '!src/js/*.js'], 'public/assets/js')
 const bundle = gulpy.bundle('resources/assets/js/*.js', 'public/assets/js', 'bundle.js')
 const images = gulpy.images('resources/assets/img/**/*', 'public/assets/img')
 const clean = gulpy.clean(['public/assets/**'])
 const copyNpm = gulpy.copyNpm('public/node_modules')
-const version = gulpy.version(['public/assets/**/*', 'public/vue/**/*'])
+const version = gulpy.version('public/assets/**/*')
+const npmVersion = gulpy.npmVersion()
+const replaceVersion = gulpy.replaceVersion('public/**/*.css', 'public')
 
 // admin
 const sassAdmin = gulpy.sass('resources/assets/admin/sass/admin.scss', 'public/assets/admin/css')
-const jsAdmin = gulpy.js(['resources/assets/admin/js/**/*'], 'public/assets/admin/js')
+const jsAdmin = gulpy.js(['resources/assets/admin/js/**/*.js'], 'public/assets/admin/js')
 
 // export
 exports.default = gulp.series(clean, gulp.parallel(sass, js, bundle, images, copyNpm, sassAdmin, jsAdmin))
 
 if (gulpy.isProduction()) {
-  exports.default = gulp.series(exports.default, version)
+  exports.default = gulp.series(exports.default, version, npmVersion, replaceVersion)
 }
 exports.watch = gulpy.watch()
---
+++ resources/assets/js/menu.js
@@ -0,0 +1,10 @@
+/* eslint-disable-next-line no-unused-vars */
+const Menu = (function () {
+  class Menu {
+    constructor () {
+      console.log('Menu')
+    }
+  }
+
+  return Menu
+})()
