702ab88d2e3c40f772e3b7b166d9a688f9bff4fb
Brybry <bryan@agence-webup.com>
2021-03-17 11:04:14 +0100

    Add configurable SEO canonical

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Request;
+use Closure;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +32,65 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->make('config')->get('app.https')) {
             $this->app->url->forceScheme('https');
         }
+
+        $this->updateCannonical();
+    }
+
+    private function getCanonicalConfig()
+    {
+        return [
+            "page" => function ($value) {
+                return $value != 1;
+            },
+        ];
+    }
+
+
+    private function updateCannonical()
+    {
+        //get current url without query params
+        $url = url()->current();
+        $trimmedAppUrl = rtrim(config('app.url'), "/");
+        //Replace request root by app.url from config (ex : replace ip by domain)
+        $url = str_replace(Request::root(), $trimmedAppUrl, $url);
+
+
+        // Replace http by https if app.https config is true
+        if (config("app.https")) {
+            $url = str_replace("http://", "https://", $url);
+        }
+        // Add trailing on domain request
+        if ($url == $trimmedAppUrl) {
+            $url = $url . "/";
+        }
+
+        // Concat with authorized query params if needed
+        if (count($getParams = $this->getAuthorizedQueryParams()) > 0) {
+            $url .= "?" . http_build_query($getParams);
+        }
+        // Set canonical
+        SEOMeta::setCanonical($url);
+    }
+
+
+    private function getAuthorizedQueryParams()
+    {
+        $getParams = [];
+        $config = $this->getCanonicalConfig();
+        // Extract only authorized getParams keys from request matching with 'getCanonicalConfig'
+        $autorizedParams = url()->getRequest()->only(array_map(function ($key) use ($config) {
+            return $config[$key] instanceof Closure ? $key : $config[$key];
+        }, array_keys($config)));
+        // Extract authorized params with value check
+        foreach ($autorizedParams as $autorizedParam => $value) {
+            if (isset($config[$autorizedParam]) && $config[$autorizedParam] instanceof Closure) {
+                if ($config[$autorizedParam]($value)) {
+                    $getParams[$autorizedParam] = $value;
+                }
+            } else {
+                $getParams[$autorizedParam] = $value;
+            }
+        }
+        return $getParams;
     }
 }
