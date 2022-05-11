66d8eaadcd5a34d513f8eafd5188017ecd802a9f
Tixie <tixie@agence-webup.com>
2022-05-11 11:49:34 +0200

    Update ESlint tooling and conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
-  'extends': [
-    'standard',
-    'plugin:vue/recommended'
+  env: {
+    browser: true, // browser global variables
+    amd: true, // define require() as global (used into gulpfile for example)
+    es2022: true // set EMACScript version
+  },
+  extends: [
+    'standard' // We use the JavaScript Standard Style convention
   ],
   rules: {
-    'vue/max-attributes-per-line': 'off'
+    // Remove errors for production env
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
--- resources/assets/js/menu.js
+++ resources/assets/js/menu.js
@@ -1,4 +1,4 @@
-/* eslint-disable-next-line no-unused-vars */
+// eslint-disable-next-line no-unused-vars
 const Menu = (function () {
   class Menu {
     constructor () {
